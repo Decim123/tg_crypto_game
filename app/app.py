6bd15ff3b93fc5fe380e31da9daf898ef76dc36a
Александр Туман <102609734+Decim123@users.noreply.github.com>
2024-08-16 12:52:23 +0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ import sqlite3
 app = Flask(__name__)
 
 def get_db_connection():
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect('app/database.db')
     conn.row_factory = sqlite3.Row
     return conn
 
